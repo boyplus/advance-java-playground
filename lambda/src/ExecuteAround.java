4ad65e8ad13257bab38d710bc8cb6d2ac1ee1310
boyplus <ict224bj@gmail.com>
2023-05-21 00:38:38 +0700

    add: ExecuteAround pattern

--- lambda/src/ExecuteAround.java
+++ lambda/src/ExecuteAround.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExecuteAround {
     public static void main(String[] args) {
+        // # Recall -> Lambda can be used only in the context of functional interface
+        // Signature: BufferReader -> String and may throw an
+        try {
+            String oneLine = processFile((BufferedReader br) -> br.readLine());
+            String twoLines = processFile((BufferedReader br) -> br.readLine() + br.readLine());
 
+            System.out.println(oneLine);
+            System.out.println("==================");
+            System.out.println(twoLines);
+
+        } catch(IOException e){
+            System.out.println("inside error");
+            System.out.println(e);
+        }
+
+    }
+
+    public static String processFile(BufferReaderProcessor p) throws IOException {
+        // We could wrap statement that will throw exception inside () of try instead of using catch
+        try(BufferedReader br = new BufferedReader(new FileReader("data.txt"));) {
+            // Process the BufferReader object
+            return p.process(br);
+        }
     }
 }
