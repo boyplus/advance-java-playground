14d03bb373b464ee3bf88ee4e955779268f8eba9
boyplus <ict224bj@gmail.com>
2023-05-21 16:15:06 +0700

    add: cast type

--- lambda/src/Main.java
+++ lambda/src/Main.java
@@ -1,3 +1,8 @@
+import predicate.Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // Example of Functional Interface and use cases
@@ -28,5 +33,44 @@ public class Main {
         // 5. Combine two values
         // (int a,int b) -> a*b
         // IntBinaryOperator
+
+        // 6. Compare two object
+        // (Apple a1, Apple a2) -> a1.weight.compareTo(a2.weight)
+        // Comparator<Apple> OR BiFunction<Apple,Apple,Integer> OR ToIntBiFunction<Apple,Apple>
+
+
+        // Exception, lambdas an functional interfaces
+        // None of the functional interface allow for a checked expection to be thrown
+        // 1. Define our onw functional interface
+        // 2. Wrap a lambda body with a try/catch block
+
+        // *Note: The method add of a List returns a "boolean" and not "void"
+        // Special void-compatibility rule
+        List<String> list = new ArrayList<>();
+        // Predicate has a boolean return
+        Predicate<String> p = (String s) -> list.add(s);
+
+        // Consumer has a void return
+        Consumer<String> b = (String s) -> list.add(s);
+
+        // Which will get executed ?
+        // The error will be ambiguous call -> need to cast to Runnable or Action
+        // execute(()->{});
+        execute((Action) () -> {} );
+        execute((Runnable) () -> {} );
+
     }
+
+    public static void execute(Runnable runnable){
+        runnable.run();
+    }
+
+    public static <T> void execute(Action<T> action){
+        action.act();
+    }
+}
+
+@FunctionalInterface
+interface Action<T>{
+    void act();
 }
